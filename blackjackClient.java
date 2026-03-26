f22c12999d6f7092aa5b8e4d81cb98b21eff98ea
Joy Janney <143513601+janneyj@users.noreply.github.com>
2026-03-26 19:05:20 +0000

    updated command line

--- blackjackClient.java
+++ blackjackClient.java
@@ -1,5 +1,7 @@
-import java.net.Socket;
+import java.io.DataOutputStream;
 import java.io.InputStream;
+import java.net.*;
+
 /*To do:
     * comment
     * Accept tcp connection from port 1706
@@ -7,6 +9,7 @@ import java.io.InputStream;
     * Close socket
     * print bye when closing socket
 */
+import java.io.OutputStream;
 
 /**
  * @author Joy Janney
@@ -27,13 +30,18 @@ public class blackjackClient{
         int n = in.read(buf);
         System.out.println(new String(buf, 0, n));*/
         System.out.println("Socket Created");
-        System.out.println("Closing socket connection in 10 seconds...");
 
+
+
+        System.out.println("Closing socket connection in 10 seconds...");
         //https://medium.com/@AlexanderObregon/building-a-countdown-timer-in-java-using-loops-for-beginners-67db5d461c18
         for(int i = 9; i > 0; i --){
             System.out.println(i + "...");
             Thread.sleep(1000);
         }
+        //https://www.geeksforgeeks.org/java/socket-programming-in-java/
+        DataOutputStream socketOutput = new DataOutputStream(s.getOutputStream());
+        socketOutput.writeUTF("Goodbye");
         s.close();
         System.out.println("Socket Closed");
     }
