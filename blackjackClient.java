142a2e0e379d9cb25200884765029034d8581f55
Joy Janney <143513601+janneyj@users.noreply.github.com>
2026-03-30 09:48:14 -0400

    added comments

--- blackjackClient.java
+++ blackjackClient.java
@@ -1,39 +1,48 @@
+//necessary imports
 import java.net.Socket;
 import java.io.InputStream;
-/*To do:
-    * comment
-    * Accept tcp connection from port 1706
-    * Print out hello when accepting socket
-    * Close socket
-    * print bye when closing socket
-*/
 
-/**
- * @author Joy Janney
- * Course:
- * Professor:
- * Date Created: 3/25/26
- * Description: 
- */
+/***********************************************************/
+/* Author: Joy Janney                                      */
+/* Major: Computer Science                                 */
+/* Creation Date: March 25th, 2026                         */
+/* Due Date: April 2nd, 2026                               */
+/* Course: CS321-01                                        */
+/* Professor Name: Professor Shimkanon                     */
+/* Assignment: Final Project Phase #1                      */
+/* Filename: blackjackClient.java                          */
+/* Purpose: This program connects to a blackjack server,   */
+/*   says hello, disconnects, and says goodbye.            */
+/***********************************************************/
 
 public class blackjackClient{
+   //details for the socket connection if static (no command line arguments)
     final public static int PORT = 1706;
     final public static String IP = "localhost";
+   
     public static void main(String[] args) throws Exception
     {
+       //creating the socket
         Socket s = new Socket(IP, PORT);
+       
+       //use is you want to read in from the server
         /*InputStream in = s.getInputStream();
         byte [] buf = new byte[1024];
         int n = in.read(buf);
         System.out.println(new String(buf, 0, n));*/
-        System.out.println("Socket Created");
-        System.out.println("Closing socket connection in 10 seconds...");
 
+       //printing out socket connection details
+        System.out.println("Socket Created");
+
+       //timing out socket in 10 seconds
+        System.out.println("Closing socket connection in 10 seconds...");
         //https://medium.com/@AlexanderObregon/building-a-countdown-timer-in-java-using-loops-for-beginners-67db5d461c18
         for(int i = 9; i > 0; i --){
             System.out.println(i + "...");
             Thread.sleep(1000);
         }
+       
+       //closing socket and printing it to user
         s.close();
         System.out.println("Socket Closed");
     }
