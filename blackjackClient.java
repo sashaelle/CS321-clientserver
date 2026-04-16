dbd5b207d088d48119d1e7ab4afeca67c3911625
Joy Janney <143513601+janneyj@users.noreply.github.com>
2026-04-16 19:17:07 +0000

    added in reading from line (string) and not chars

--- blackjackClient.java
+++ blackjackClient.java
@@ -5,17 +5,15 @@
 /* Due Date: April 2nd, 2026                               */
 /* Course: CS321-01                                        */
 /* Professor Name: Professor Shimkanon                     */
-/* Assignment: Final Project Phase #1                      */
+/* Assignment: Final Project Phase #2                      */
 /* Filename: blackjackClient.java                          */
 /* Purpose: This program connects to a blackjack server,   */
 /*   says hello, disconnects, and says goodbye.            */
 /***********************************************************/
 
 /* TO DO:
-    * accept input from the server
     * comment
     * use score function
-    * add in initial 2 card
     * add in dealer hand
  */
 
@@ -28,7 +26,7 @@ import java.util.Random;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.HashMap;
-
+import java.io.BufferedReader;
 
 public class blackjackClient{
     //details for the socket connection if static (no command line arguments)
@@ -47,14 +45,23 @@ public class blackjackClient{
         String IP = args[0];
         Socket s = new Socket(IP, Port);
         System.out.println("Connected opened to " + IP + " at port " + Port);
-        OutputStreamWriter output = new OutputStreamWriter(s.getOutputStream(), "UTF8");
-        InputStreamReader input = new InputStreamReader(s.getInputStream(), "UTF8");
 
+        //setting up reading and writing from the socket
+        OutputStreamWriter output = new OutputStreamWriter(s.getOutputStream(), "UTF8");
+        BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream(), "UTF8"));
+
+        //get rid of r when score funciton is implemented
         Random r = new Random();
+        int playerScore = r.nextInt(25);
+        int dealerScore = r.nextInt(20);
+
+        //for getting input from user
         Scanner std_in = new Scanner(System.in);
 
         int option;//user option input
         boolean continueGame = true;//whether to start a new game or not
+
+        //should have functions moved to library later
         createHashMaps();
 
         System.out.println("Welcome to Blackjack");
@@ -63,31 +70,44 @@ public class blackjackClient{
             output.write("Deal\n", 0, 5);
             output.flush();
 
-            //hand for the game
-            ArrayList<char[]> hand = new ArrayList<char[]>();
+            //new player hand for the game
+            ArrayList<char[]> playerHand = new ArrayList<char[]>();
+            ArrayList<char[]> dealerHand = new ArrayList<char[]>();
             System.out.println("Waiting on intial 2 cards from server");
             //do NOT move out, need new memory address
+            addCard(playerHand, input);
+            addCard(playerHand, input);
 
-            //do NOT move out, need new memory address
-            addCard(hand, input);
+            addCard(dealerHand, input);
 
             //whether to hit or hold
             boolean hit = true;
             //continue while they want to hit
             //TO DO: compare to score
-            while(hit == true){
+            while(hit == true & playerScore < 21){
 
                 //print out user hand
-                for(char[] card : hand){
+                System.out.println("**********");
+                System.out.println("Your hand");
+                for(char[] card : playerHand){
                     System.out.println(cardTranslator(card));
                 }
-                //TO DO: print out dealer hand
+                System.out.println("**********");
+
+                //print out dealer hand
+                System.out.println("**********");
+                System.out.println("Dealer hand");
+                System.out.println("Hidden card");
+                System.out.println(cardTranslator(dealerHand.get(0)));
+                System.out.println("**********");
 
                 //ask user to hit or hold
                 boolean validInput = false;
                 while(!validInput){
                     System.out.println("(1) Hit\n(2) Hold");
                     option = std_in.nextInt();
+
+                    //if hit, ask server for new card
                     if (option == 1){
                         output.write("Hit\n", 0, 4);
                         output.flush();
@@ -95,8 +115,10 @@ public class blackjackClient{
                         validInput = true;
                          //do NOT move out, need new memory address
                         System.out.println("Waiting on new card from server");
-                        addCard(hand, input);
+                        addCard(playerHand, input);
                     }
+
+                    //if hold, end game
                     else if (option == 2){
                         output.write("Hold\n", 0, 5);
                         output.flush();
@@ -111,16 +133,17 @@ public class blackjackClient{
                 //TO DO: calculate score of the hand
             }
             //TO DO: call score function to determine winner
-            if(r.nextInt(2) == 0){
-                System.out.println("Congrats you won!!!");
-                System.out.println("Dealer hand score: " + (r.nextInt(10)));
-                System.out.println("Your hand score: " + (r.nextInt(10) + 11));
+            if(playerScore > 21){
+                System.out.println("Bust!");
+            }
+            else if(playerScore > dealerScore){
+                System.out.println("Congrats you won!");
             }
             else{
-                System.out.println("Better luck next time :(");
-                System.out.println("Dealer hand score: " + (r.nextInt(10) + 11));
-                System.out.println("Your hand score: " + r.nextInt(10));
+                System.out.println("You lost!");
             }
+            System.out.println("Dealer hand score: " + dealerScore);
+            System.out.println("Your hand score: " + playerScore);
 
             //ask user if they want to start a new game
             boolean validInput = false;
@@ -190,22 +213,26 @@ public class blackjackClient{
         return nameNumber + " of " + nameSuite;
     }
 
-    public static void addCard(ArrayList<char[]> hand, InputStreamReader input) throws Exception{
-            char[] inputCard = {'x','x','x','x', 'x','x','x','x', 'x','x','x','x', 'x','x','x','x', 'x','x','x','x',
-                'x','x','x','x', 'x','x','x','x', 'x','x','x','x', 'x','x','x','x', 'x','x','x','x'
-            };
-            input.read(inputCard);
+    /****************************************************************************/
+    /* Function name: addCard                                                   */
+    /* Description: Adds to a hand from a given input                           */
+    /* Parameters: ArrayList<char[]> - hand: the hand to save the card to       */
+    /*              InputStreamReader input: the socket input                   */
+    /* Return Value: void                                                       */
+    /****************************************************************************/
+    public static void addCard(ArrayList<char[]> hand, BufferedReader input) throws Exception{
 
-            String stringCard = new String(inputCard);
-            int index = stringCard.indexOf(',');
+            String inString;
+            inString = input.readLine();
+            System.out.println("Input: " + inString);
+
+            int index = inString.indexOf(',');
             if(index <= 0){
-                System.out.println("ERROR: " + stringCard);
+                System.out.println("ERROR: " + inString);
                 return;
             }
-
             //add card to hand
-            char[] card = {inputCard[index-1], inputCard[index+2]};
-            System.out.println("Card: " + new String(card) + "\nIndex of: " + index);
+            char[] card = {inString.charAt(index-1), inString.charAt(index+2)};
             hand.add(card);
     }
 }
